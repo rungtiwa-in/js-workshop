7c386bc1e9ec2da84783e0d94b119347de4e16de
Rungtiwa_in <rungtiwa.inha@gmail.com>
2020-06-19 15:23:19 +0700

    workshop js

---
+++ myscript.js
@@ -0,0 +1,66 @@
+// alert('Hello JS')
+// console.log("Hello JS")
+// let a = 10
+// let b = 20
+// let c = a + b
+// console.log("a + b =", c)
+
+// let message = 'JS', num = 10, isTrue = true
+// console.log(message, num, isTrue)
+
+// let name = 'noon'
+// let human = name
+// console.log(human)
+
+// let myMoney = 1000
+// let myMom = 'Mommmm'
+// let myDad = 'Dadddd'
+// let myAddress = 'Maxxi Condo'
+// let ageOfUniverse = 10000000
+// console.log(myMoney, myMom, myAddress, ageOfUniverse)
+
+// let msg1 = 'baac', msg2 = 'bkk';
+// let msg3 = `i'm work at ${msg1} in ${msg2}`
+// console.log(msg3)
+
+// let _null = null
+// console.log(_null)
+
+// let _undefined = undefined
+// console.log(_undefined)
+
+// console.log(typeof msg3) //string
+// console.log(typeof myMoney) //number
+
+// let studentName = 'Rungtiwa'
+// let studentAge = 27
+// let studentAddress = 'Maxxi';
+// let studentData = `Name : ${studentName} \
+// Age : ${studentAge} \
+// Address : ${studentAddress}`
+// console.log(studentData)
+
+// let a = 1
+// let b = 1
+// let c = ++a
+// let d = b++
+// console.log(a, b, c, d)
+
+
+// let message = prompt("what your name ?") 
+// if(message=="noon") alert("yes!")
+// else alert("no")
+
+
+// let score = prompt("what your score ?")
+// if(score>=80) alert('A')
+// else if(score>=70 && score<80) alert('B')
+// else if(score>=60 && score<70) alert('C')
+// else if(score>=50 && score<60) alert('D')
+// else if(score<50) alert('F')
+// else alert('other')
+
+
+age = prompt('how old are you ?')
+let price = (age < 18) ? 2000 : 3500
+alert(price)
